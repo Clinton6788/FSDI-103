a5868e2178e63b7a977cca28171a895f3bae92cf
Clinton6788 <clinton.g.hockenberry@gmail.com>
2024-08-25 19:04:03 -0400

    Assignment 1 Actual

--- scripts/script.js
+++ scripts/script.js
@@ -47,3 +47,27 @@ let numberOne = document.getElementById("numberOne").value;
 let numberTwo = document.getElementById("numberTwo").value;
 document.getElementById("sum").innerHTML=numberOne - numberTwo;
 }
+// Need to add some if statements for operation, but should be fairly simple
+
+
+// ASSIGNMENT 1
+
+let storyName = "John";
+let physicalAction = "squat";
+let companyName = "coding coders llc";
+let dailyAction = "making coffee";
+let noun = "dog";
+
+const codingLanguage = "python"
+
+//Version 2
+storyName = "Greg";
+physicalAction = "yawn";
+companyName = "McDonalds";
+dailyAction = "driving"
+noun = "paper towels"
+
+console.log("There was once a coder named " + storyName + ".")
+console.log(storyName + " liked to " + physicalAction + " every morning before starting work at " + companyName + ".")
+console.log("His/her favorite part of the day was " + dailyAction + ", especially when dealing with " + codingLanguage + ".")
+console.log("However, his/her " + noun + " always seemed to get in the way when using " + codingLanguage + ".")
